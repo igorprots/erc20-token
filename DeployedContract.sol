2d9a2a8d64041a523c5dba0e9a0482658ae25a4c
Igor Protsenko <protsxe51@gmail.com>
2020-07-08 16:47:11 +0300

    add deployed contract

---
+++ DeployedContract.sol
@@ -0,0 +1,181 @@
+/**
+ *Submitted for verification at Etherscan.io on 2020-07-02
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.8;
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+
+contract ERC20Standard {
+    using SafeMath for uint256;
+    uint256 public totalSupply;
+    string public name;
+    uint256 public decimals;
+    string public symbol;
+    uint256 public burningDate;
+    uint256 length = 2;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+    mapping (address => uint256) counter;
+    mapping (address => bool) owners;
+	mapping (address => mapping (address => bool)) ownerChoice;
+
+
+
+	address constant own1 = 0xDB3Da15110cD37844515471f883A663D0239d174;
+	address constant own2 = 0xf3CA95E256effCE14c6674641510A13449548632;
+
+
+
+    //Fix for short address attack against ERC20
+    modifier onlyPayloadSize(uint256 size) {
+        assert(msg.data.length == size + 4);
+        _;
+    }
+    	modifier onlyOwner() {
+		require(owners[msg.sender] == true);
+		_;
+	}
+
+	function ownersBalance() public {
+		owners[own1] = true;
+		owners[own2] = true;
+		balances[own1] = totalSupply/2;
+		balances[own2] = totalSupply - balances[own1];
+	}
+
+	function isOwner(address _owner) public view returns (bool Owner) {
+		return owners[_owner];
+	}
+
+	function isOwnerChoice(address _owner, address _newOwner) public view returns (bool OwnerChoice) {
+		return ownerChoice[_owner][_newOwner];
+	}
+
+	function addOwner(address _newOwner) public onlyOwner {
+		require(ownerChoice[msg.sender][_newOwner] == false && owners[_newOwner] == false);
+			ownerChoice[msg.sender][_newOwner] = true;
+			counter[_newOwner]++;
+			if (counter[_newOwner] > length/2) {
+				owners[_newOwner] = true;
+				length++;
+				counter[_newOwner] = 0;
+			}
+	}
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _recipient, uint256 _value)
+        public
+        onlyPayloadSize(2 * 32)
+    {
+        require(balances[msg.sender] >= _value && _value > 0);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_recipient] = balances[_recipient].add(_value);
+        emit Transfer(msg.sender, _recipient, _value);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public {
+        require(
+            balances[_from] >= _value &&
+                allowed[_from][msg.sender] >= _value &&
+                _value > 0
+        );
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+    }
+
+    function allowance(address _spender, address _owner)
+        public
+        view
+        returns (uint256 balance)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    function burn(uint256 _value) external {
+        require(now > burningDate, "Coins can't be burned now");
+        require(
+            balances[msg.sender] >= _value,
+            "Account balance is not enought to perform coins burning"
+        );
+        balances[msg.sender] -= _value;
+        totalSupply -= _value;
+    }
+
+    //Event which is triggered to log all transfers to this contract's event log
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    //Event which is triggered whenever an owner approves a new allowance for a spender.
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+}
+
+contract BoomToken is ERC20Standard {
+    address public owner;
+    constructor() public {
+        owner = msg.sender;
+        decimals = 4;
+        totalSupply = 1000000 * 10 ** decimals;
+        name = "BoomToken";
+        symbol = "BMT";
+        balances[msg.sender] = totalSupply;
+        burningDate = now + 10 hours;
+        emit Transfer(address(0), owner, totalSupply);
+    }
+}
--- contracts/DividendToken.sol
+++ contracts/DividendToken.sol
@@ -7,14 +7,14 @@ import '../node_modules/@openzeppelin/contracts/utils/EnumerableMap.sol';
 
 
 
-contract Tokenv1 is ERC20 {
+contract MyToken is ERC20 {
 
    using EnumerableMap for EnumerableMap.UintToAddressMap;
    using SafeMath for uint256;
 
-    string public constant name = "Divident Token";
-    string public constant symbol = "DET";
-    uint8 public constant decimals = 3;
+    string public override name = "Divident Token";
+    string public  override symbol = "DET";
+    uint8 public override decimals = 3;
 
     uint256 _totalSupply = 1000000000;
 
@@ -22,52 +22,56 @@ contract Tokenv1 is ERC20 {
 
     uint256 public tokenPrice;
 
-    uint256 public constant amountOfEthereum = this.balance; 
+    uint256 public constant amountOfEthereum = balanceOf; 
 
     mapping (address => mapping (address => uint256)) allowed;
 
-    EnumerableMap.UintToAddressMap tokensBalances;
-    EnumerableMap.UintToAddressMap ethereumBalances;
+    EnumerableMap.UintToAddressMap tokensbalanceOf;
+    EnumerableMap.UintToAddressMap ethereumbalanceOf;
  
-    function Tokenv1() public {
-        owner = msg.sender;
-        tokensBalances.insert(owner,_totalSupply);
+    // function Tokenv1() public {
+    //     owner = msg.sender;
+    //     tokensbalanceOf.insert(owner,_totalSupply);
+    // }
+
+    modifier onlyPayloadSize(uint256 size) {
+        assert(msg.data.length == size + 4);
+        _;
     }
 
+    // function totalSupply() public view returns (uint256 totalSupply) {
+    //     totalSupply = _totalSupply;
+    //     return totalSupply;
+    // }
 
-    function totalSupply() public view returns (uint256 totalSupply) {
-        totalSupply = _totalSupply;
-        return totalSupply;
-    }
 
+    // function balanceOfOf(address _owner) public view returns (uint256 balanceOf) {
+    //     return balanceOf[_owner];
+    // }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
+    // function transfer(address _recipient, uint256 _value) public onlyPayloadSize(2 * 32){
+    //     require(balanceOf[msg.sender] >= _value && _value > 0);
+    //     balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    //     balanceOf[_recipient] = balanceOf[_recipient].add(_value);
+    //     emit Transfer(msg.sender, _recipient, _value);
+    // }
 
-    function transfer(address _recipient, uint256 _value) public onlyPayloadSize(2 * 32){
-        require(balances[msg.sender] >= _value && _value > 0);
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_recipient] = balances[_recipient].add(_value);
-        emit Transfer(msg.sender, _recipient, _value);
-    }
+    // function transferFrom(address _from,address _to,uint256 _value) public {
+    //     require(balanceOf[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+    //     balanceOf[_to] = balanceOf[_to].add(_value);
+    //     balanceOf[_from] = balanceOf[_from].sub(_value);
+    //     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    //     emit Transfer(_from, _to, _value);
+    // }
 
-    function transferFrom(address _from,address _to,uint256 _value) public {
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
-    }
+    // function approve(address _spender, uint256 _value) public {
+    //     allowed[msg.sender][_spender] = _value;
+    //     emit Approval(msg.sender, _spender, _value);
+    // }
 
-    function approve(address _spender, uint256 _value) public {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-    }
-
-    function allowance(address _spender, address _owner) public view returns (uint256 balance){
-        return allowed[_owner][_spender];
-    }
+    // function allowance(address _spender, address _owner) public view returns (uint256 balanceOf){
+    //     return allowed[_owner][_spender];
+    // }
 
 
     receive() external payable{
@@ -80,19 +84,19 @@ contract Tokenv1 is ERC20 {
         
         tokenPrice = amountOfEthereum.div(_totalSupply);
 
-        for(uint i = 0; i < tokensBalances.size(); i++){
-            value = tokensBalances.getValueByIndex(i);
-            key = tokensBalances.getKeyByIndex(i);
+        for(uint i = 0; i < tokensbalanceOf.size(); i++){
+            value = tokensbalanceOf.getValueByIndex(i);
+            key = tokensbalanceOf.getKeyByIndex(i);
 
-            ethereumBalances.insert(key, value.mul(tokenPrice));
+            ethereumbalanceOf.insert(key, value.mul(tokenPrice));
 
             key.transfer(value.mul(tokenPrice));
         }
     }
 
-    function balanceOfETH(address _owner) public view returns (uint256 balance) {
-        balance = ethereumBalances.get(_owner);
-        return balance;
+    function balanceOfOfETH(address _owner) public view returns (uint256 balanceOf) {
+        balanceOf = ethereumbalanceOf.get(_owner);
+        return balanceOf;
     }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
